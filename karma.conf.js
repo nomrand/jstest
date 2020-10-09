cc83e0252f026ee4d0eb6c23ebfc27e11870ade9
nomrand <42882840+nomrand@users.noreply.github.com>
2020-10-09 13:56:21 +0900

    add jquery 3.4.1

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,8 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'js/**/*.js',
-      'test/**/*.js'
+      'test/**/*.js',
+      "https://code.jquery.com/jquery-3.4.1.min.js"
     ],
 
 
